1ec551ff9c3a137d5667125b30f66e4cdff5f737
AgathaLice <miguelfabrisd@gmail.com>
2025-07-18 01:59:50 -0300

    Revert "Um service para busca e resultado, terminar no final o controller do resultado"
    This reverts commit 044426ef5575326f7eefcb38b65250981c61f1b5.

--- src/main/java/br/edu/ifpr/Busca/BuscaController.java
+++ src/main/java/br/edu/ifpr/Busca/BuscaController.java
@@ -1,14 +1,12 @@
 package br.edu.ifpr.Busca;
 
 
-import com.sun.javafx.fxml.FXMLLoaderHelper;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
 
+import br.edu.ifpr.Busca.BuscaService;
 import javafx.scene.control.TextField;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.util.List;
 
@@ -21,24 +19,14 @@ public class BuscaController {
     @FXML
     private ChoiceBox<String> buscaChoice;
 
-    @Setter
-    @Getter
     private String textoBusca = buscaField.getText();
-    @Setter
-    @Getter
     private String itemSelecionado = buscaChoice.getValue();
 
-    @Setter
-    private BuscaEResultadoService buscaEResultadoService;
-    // TODO -> Lembrar de instanciar isso na Main
+    private BuscaService buscaService = new BuscaService();
 
-    // TODO -> Adicionar função ao botão de buscar
+
     public void buscar(String textoBusca, String getItemSelecionado){
-        buscaEResultadoService.buscar(textoBusca, getItemSelecionado);
-    }
-
-    public void abrirResultado(List<?> listaResultados){
-        // Abre a outra tela, passando o resultado
+        buscaService.buscar(textoBusca, getItemSelecionado);
     }
 
     @FXML
--- target/classes/out/production/Sistema-de-Adocao-de-Animais/README.md
+++
@@ -1,5 +0,0 @@
-# Sistema-de-br.edu.ifpr.Adocao-de-Animais
-
-Protótipo de sistema de adoção de animais para a matéria de POO.
-
-Diagrama de classes: https://drive.google.com/file/d/1lIx9xJq9zQiB6t2yyrLnOzwWWjt_t-57/view?usp=sharing
