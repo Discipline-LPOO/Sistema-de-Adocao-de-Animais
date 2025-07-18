0f1ee5a33dd2d561ecfe7d3f8935c86bc97c119a
Yasmim <108767728+Tybika@users.noreply.github.com>
2025-07-18 17:54:14 -0300

    refactor: include module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module br.edu.ifpr {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires jakarta.persistence;
+    requires static lombok;
+    requires java.desktop;
+
+    opens br.edu.ifpr to javafx.fxml;
+    exports br.edu.ifpr;
+}
