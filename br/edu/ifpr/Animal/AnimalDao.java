9e4725a9c05146995d0b6f0173b6495ec2189a48
AgathaLice <miguelfabrisd@gmail.com>
2025-07-17 22:10:02 -0300

    Add query by name
    Adiciona a query de adotante por nome

--- br/edu/ifpr/Animal/AnimalDao.java
+++ br/edu/ifpr/Animal/AnimalDao.java
@@ -3,4 +3,9 @@ package br.edu.ifpr.Animal;
 import br.edu.ifpr.Dao.Dao;
 
 public class AnimalDao extends Dao<Animal> {
+    public static void selectPorNome(String textoBusca) {
+    }
+
+    public static void selectPorDataAcolhimento(String textoBusca) {
+    }
 }
